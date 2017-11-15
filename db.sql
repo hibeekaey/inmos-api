3fcd5463f9b2e4a0504bb49c73431065e8c6a02d
Hibeekaey <hibeekaey@yahoo.com>
2017-11-15 09:34:41 +0100

    Add db.sql

---
+++ db.sql
@@ -0,0 +1,53 @@
+-- Database: inmos
+
+-- DROP DATABASE IF EXISTS inmos;
+
+CREATE DATABASE inmos;
+
+COMMENT ON DATABASE inmos
+  IS 'inmos database';
+
+-- Table: stores_table
+
+-- DROP TABLE IF EXISTS stores_table CASCADE;
+
+CREATE TABLE IF NOT EXISTS stores_table
+(
+
+);
+
+-- Table: vendors_table
+
+-- DROP TABLE IF EXISTS vendors_table CASCADE
+
+CREATE TABLE IF NOT EXISTS vendors_table
+(
+
+);
+
+-- Table: inventory_table
+
+-- DROP TABLE IF EXISTS inventory_table CASCADE
+
+CREATE TABLE IF NOT EXISTS inventory_table
+(
+
+);
+
+-- Table: batch_table
+
+-- DROP TABLE IF EXISTS batch_table CASCADE
+
+CREATE TABLE IF NOT EXISTS batch_table
+(
+
+);
+
+-- Table: sales_table
+
+-- DROP TABLE IF EXISTS sales_table CASCADE
+
+CREATE TABLE IF NOT EXISTS sales_table
+(
+
+);
