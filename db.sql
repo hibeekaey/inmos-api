bbc58e7275f933d2c9fa4ab039c37a5b68bb58c5
Hibeekaey <hibeekaey@yahoo.com>
2017-11-20 12:08:43 +0100

    DB setup

--- db.sql
+++ db.sql
@@ -2,17 +2,16 @@
 
 -- DROP DATABASE IF EXISTS inmos;
 
-CREATE DATABASE inmos;
+-- CREATE DATABASE inmos;
 
-COMMENT ON DATABASE inmos
-  IS 'inmos database';
+-- COMMENT ON DATABASE inmos
+--  IS 'inmos database';
 
 -- Table: store
 
 -- DROP TABLE IF EXISTS store CASCADE;
 
-CREATE TABLE IF NOT EXISTS store
-(
+CREATE TABLE IF NOT EXISTS store (
   store_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   store_name TEXT NOT NULL UNIQUE,
   password TEXT NOT NULL,
@@ -23,8 +22,7 @@ CREATE TABLE IF NOT EXISTS store
 
 -- DROP TABLE IF EXISTS vendor CASCADE
 
-CREATE TABLE IF NOT EXISTS vendor
-(
+CREATE TABLE IF NOT EXISTS vendor (
   vendor_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   vendor_name TEXT NOT NULL UNIQUE,
   contact JSONB NOT NULL
@@ -34,24 +32,22 @@ CREATE TABLE IF NOT EXISTS vendor
 
 -- DROP TABLE IF EXISTS stock CASCADE
 
-CREATE TABLE IF NOT EXISTS stock
-(
+CREATE TABLE IF NOT EXISTS stock (
   stock_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   stock_name TEXT NOT NULL,
-  category TEXT NOT NULL,
+  category TEXT NOT NULL
 );
 
 -- Table: inventory
 
 -- DROP TABLE IF EXISTS inventory CASCADE
 
-CREATE TABLE IF NOT EXISTS inventory
-(
+CREATE TABLE IF NOT EXISTS inventory (
   stock_id UUID NOT NULL REFERENCES stock ON DELETE CASCADE,
   store_id UUID NOT NULL REFERENCES store ON DELETE CASCADE,
   quantity INT DEFAULT 0 CHECK (quantity > 0),
   cost_price MONEY DEFAULT 0.00 CHECK (cost_price > 0.00),
-  selling_price MONEY DEFAULT 0.00 CHECK (selling_price > 0.00)
+  selling_price MONEY DEFAULT 0.00 CHECK (selling_price > 0.00),
   PRIMARY KEY (stock_id, store_id)
 );
 
@@ -59,22 +55,20 @@ CREATE TABLE IF NOT EXISTS inventory
 
 -- DROP TABLE IF EXISTS supplies CASCADE
 
-CREATE TABLE IF NOT EXISTS supplies
-(
+CREATE TABLE IF NOT EXISTS supplies (
   store_id UUID NOT NULL REFERENCES store ON DELETE CASCADE,
   vendor_id UUID NOT NULL REFERENCES vendor ON DELETE RESTRICT,
   stock_id UUID NOT NULL REFERENCES stock ON DELETE CASCADE,
   quantity INT DEFAULT 0 CHECK (quantity > 0),
   cost_price MONEY DEFAULT 0.00 CHECK (cost_price > 0.00),
-  date_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  date_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
  
 -- Table: sales
 
 -- DROP TABLE IF EXISTS sales CASCADE
 
-CREATE TABLE IF NOT EXISTS sales
-(
+CREATE TABLE IF NOT EXISTS sales (
   store_id UUID NOT NULL REFERENCES store ON DELETE CASCADE,
   stock_id UUID NOT NULL REFERENCES stock ON DELETE CASCADE,
   quantity INT DEFAULT 0 CHECK (quantity > 0),
