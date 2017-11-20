8fc15489a5341100335172c11d152da8394079c5
Hibeekaey <hibeekaey@yahoo.com>
2017-11-20 10:32:09 +0100

    DB setup

--- db.sql
+++ db.sql
@@ -7,51 +7,77 @@ CREATE DATABASE inmos;
 COMMENT ON DATABASE inmos
   IS 'inmos database';
 
--- Table: stores_table
+-- Table: store
 
--- DROP TABLE IF EXISTS stores_table CASCADE;
+-- DROP TABLE IF EXISTS store CASCADE;
 
-CREATE TABLE IF NOT EXISTS stores_table
+CREATE TABLE IF NOT EXISTS store
 (
   store_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  contact JSONB
+  store_name TEXT NOT NULL UNIQUE,
+  password TEXT NOT NULL,
+  contact JSONB NOT NULL
 );
 
--- Table: vendors_table
+-- Table: vendor
 
--- DROP TABLE IF EXISTS vendors_table CASCADE
+-- DROP TABLE IF EXISTS vendor CASCADE
 
-CREATE TABLE IF NOT EXISTS vendors_table
+CREATE TABLE IF NOT EXISTS vendor
 (
   vendor_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  contact JSONB
+  vendor_name TEXT NOT NULL UNIQUE,
+  contact JSONB NOT NULL
 );
 
--- Table: inventory_table
+-- Table: stock
 
--- DROP TABLE IF EXISTS inventory_table CASCADE
+-- DROP TABLE IF EXISTS stock CASCADE
 
-CREATE TABLE IF NOT EXISTS inventory_table
+CREATE TABLE IF NOT EXISTS stock
 (
-  stock_id UUID PRIMARY KEY DEFAULT gen_random_uuid()
+  stock_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  stock_name TEXT NOT NULL,
+  category TEXT NOT NULL,
 );
 
--- Table: batch_table
+-- Table: inventory
 
--- DROP TABLE IF EXISTS batch_table CASCADE
+-- DROP TABLE IF EXISTS inventory CASCADE
 
-CREATE TABLE IF NOT EXISTS batch_table
+CREATE TABLE IF NOT EXISTS inventory
 (
-  batch_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  date_time TIMESTAMP
+  stock_id UUID NOT NULL REFERENCES stock ON DELETE CASCADE,
+  store_id UUID NOT NULL REFERENCES store ON DELETE CASCADE,
+  quantity INT DEFAULT 0 CHECK (quantity > 0),
+  cost_price MONEY DEFAULT 0.00 CHECK (cost_price > 0.00),
+  selling_price MONEY DEFAULT 0.00 CHECK (selling_price > 0.00)
+  PRIMARY KEY (stock_id, store_id)
 );
 
--- Table: sales_table
+-- Table: supplies
 
--- DROP TABLE IF EXISTS sales_table CASCADE
+-- DROP TABLE IF EXISTS supplies CASCADE
 
-CREATE TABLE IF NOT EXISTS sales_table
+CREATE TABLE IF NOT EXISTS supplies
 (
-  sales_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  date_time TIMESTAMP
+  store_id UUID NOT NULL REFERENCES store ON DELETE CASCADE,
+  vendor_id UUID NOT NULL REFERENCES vendor ON DELETE RESTRICT,
+  stock_id UUID NOT NULL REFERENCES stock ON DELETE CASCADE,
+  quantity INT DEFAULT 0 CHECK (quantity > 0),
+  cost_price MONEY DEFAULT 0.00 CHECK (cost_price > 0.00),
+  date_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+);
+ 
+-- Table: sales
+
+-- DROP TABLE IF EXISTS sales CASCADE
+
+CREATE TABLE IF NOT EXISTS sales
+(
+  store_id UUID NOT NULL REFERENCES store ON DELETE CASCADE,
+  stock_id UUID NOT NULL REFERENCES stock ON DELETE CASCADE,
+  quantity INT DEFAULT 0 CHECK (quantity > 0),
+  selling_price MONEY DEFAULT 0.00 CHECK (selling_price > 0.00),
+  date_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
