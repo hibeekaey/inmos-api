f3ec4b3a3f6f64f3aff2f9e7a07cf1c1120ebdf9
Hibeekaey <hibeekaey@yahoo.com>
2017-12-05 18:04:27 +0100

    Update mail

--- googleapis_auth.js
+++ googleapis_auth.js
@@ -17,7 +17,7 @@ const googleAuth = require('google-auth-library')
 
 // If modifying these scopes, delete your previously saved credentials
 // at ~/.credentials/gmail-nodejs-quickstart.json
-const SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+const SCOPES = ['https://www.googleapis.com/auth/gmail.readonly', 'https://mail.google.com/']
 const TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
     process.env.USERPROFILE) + '/.credentials/'
 const TOKEN_PATH = TOKEN_DIR + 'gmail-nodejs-quickstart.json'
--- routes/user.js
+++ routes/user.js
@@ -22,6 +22,9 @@ const hash = require('../lib/crypt')
 // access db lib
 const db = require('../lib/db')
 
+// mail lib
+const mail = require('../lib/mail')
+
 // middleware that is specific to this router
 router.use((req, res, next) => {
   console.log('Accessing user endpoint')
@@ -49,12 +52,15 @@ router.get('/', (req, res) => {
       // hash password
       let password = hash(req.body.password, process.env.SALT, 100000, 32, 'sha512').toString('hex')
 
-      client.query('INSERT INTO store (store_name, password, contact) VALUES ($1, $2, $3)', [req.body.store_name, password, req.body.contact], (err, result) => {
+      client.query('INSERT INTO store (store_name, password, contact) VALUES ($1, $2, $3) RETURNING store_id, contact', [req.body.store_name, password, req.body.contact], (err, result) => {
         done()
 
         if (err) {
           return db.error(res, err, 'store registration failed')
         }
+
+        // send store_id to user's mail
+        mail(result.rows[0].contact.email[0], 'Store Registration Completed', result.rows[0].store_id)
 
         res.status(200).json({'status': 'success', 'message': 'store registration completed'})
       })
