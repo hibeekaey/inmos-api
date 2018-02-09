5fe218f9393980cb848972b1e197b884196f1843
Hibeekaey <hibeekaey@yahoo.com>
2018-02-09 02:57:23 +0100

    Enable CORS

--- server.js
+++ server.js
@@ -18,6 +18,13 @@ const Keygrip = require('keygrip')
 // initialize app
 const app = express()
 
+// enable CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+})
+
 app.use(bodyParser.json()) // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
