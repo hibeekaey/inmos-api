02376f099a80d85cecea4fc4f6953a0ac38b3f6c
Hibeekaey <hibeekaey@yahoo.com>
2018-02-11 15:24:50 +0100

    Update

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const Keygrip = require('keygrip')
 const app = express()
 
 // enable CORS
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*")
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
   next()
