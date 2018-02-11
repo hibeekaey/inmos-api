fc14b3815af22c6196adaf5589e0fb5bb263db14
Hibeekaey <hibeekaey@yahoo.com>
2018-02-11 16:14:29 +0100

    Update headers

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const app = express()
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "http://localhost:4200")
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-  res.header("Access-Control-Allow-Credientials", "true")
+  res.header("Access-Control-Allow-Credentials", "true")
   next()
 })
 
