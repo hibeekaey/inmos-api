77fc15cdda26c880d2b8daa0ec3b69a4ff03fd86
Hibeekaey <hibeekaey@yahoo.com>
2018-02-11 16:07:09 +0100

    Update CORS

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const app = express()
 
 // enable CORS
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Origin", "http://localhost:4200")
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
   next()
 })
